5c0d60e6031a322828a178e476256b320fe4e592
Luka Lazic <luka.web.php@gmail.com>
2022-09-15 19:43:19 +0200

    add tailwind; start adding header

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      dark:{
+        default: "#000000"
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
