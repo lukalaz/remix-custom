aa6a412ba869ac6d5a2bccce446f09c5f233c2c3
Luka Lazic <luka.web.php@gmail.com>
2022-09-16 17:01:11 +0200

    implementing sticky menu test setup

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -69,10 +69,7 @@ const Header = () => {
                     </a>
                   </li>
                   <li className="relative group submenu-item">
-                    <a
-                      href="javascript:void(0)"
-                      className=" text-base text-black group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:pl-0 lg:pr-4 flex mx-8 lg:mr-0 lg:ml-8 xl:ml-12 relative after:absolute after:w-2 after:h-2 after:border-b-2 after:border-r-2 after:border-current after:rotate-45 lg:after:right-0 after:right-1 after:top-1/2 after:translate-y-[-50%] after:mt-[-2px]"
-                    >
+                    <a className=" text-base text-black group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:pl-0 lg:pr-4 flex mx-8 lg:mr-0 lg:ml-8 xl:ml-12 relative after:absolute after:w-2 after:h-2 after:border-b-2 after:border-r-2 after:border-current after:rotate-45 lg:after:right-0 after:right-1 after:top-1/2 after:translate-y-[-50%] after:mt-[-2px]">
                       Pages
                     </a>
                     <div className=" submenu hidden relative lg:absolute w-[250px] top-full lg:top-[110%] left-0 rounded-sm lg:shadow-lg p-4 lg:block lg:opacity-0 lg:invisible group-hover:opacity-100 lg:group-hover:visible lg:group-hover:top-full bg-white transition-[top] duration-300">
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,58 @@
 import Header from "~/components/Header";
 
 export default function Index() {
-  return <div className="h-full">TRADA</div>;
+  return (
+    <div className="h-full">
+      TRADA <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+      TRADA <br />
+      TRADE <br />
+    </div>
+  );
 }
