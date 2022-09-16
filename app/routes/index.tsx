892692817e3f6eae43f278105774b176094cd939
Luka Lazic <luka.web.php@gmail.com>
2022-09-16 02:15:38 +0200

    remove whitespaces

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -25,27 +25,13 @@ export default function Index() {
               </button>
               <nav
                 id="navbarCollapse"
-                className="absolute py-5 lg:py-0 lg:px-4 xl:px-6 bg-white
-                  lg:bg-transparent
-                  shadow-lg rounded-lg max-w-[250px] w-full
-                  lg:max-w-full lg:w-full
-                  right-4 top-full hidden
-                  lg:block lg:static lg:shadow-none
-                "
+                className="absolute py-5 lg:py-0 lg:px-4 xl:px-6 bg-white lg:bg-transparent shadow-lg rounded-lg max-w-[250px] w-full lg:max-w-full lg:w-full right-4 top-full hidden lg:block lg:static lg:shadow-none"
               >
                 <ul className="blcok lg:flex">
                   <li className="relative group">
                     <a
                       href="#home"
-                      className="
-                        menu-scroll
-                        text-base text-black
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:px-0
-                        flex mx-8
-                        lg:mr-0
-                      "
+                      className="menu-scroll text-base text-black group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:px-0 flex mx-8 lg:mr-0"
                     >
                       Home
                     </a>
@@ -53,16 +39,7 @@ export default function Index() {
                   <li className="relative group">
                     <a
                       href="#about"
-                      className="
-                        menu-scroll
-                        text-base text-black
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:px-0
-                        flex mx-8
-                        lg:mr-0 lg:ml-8
-                        xl:ml-12
-                      "
+                      className=" menu-scroll text-base text-black group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:px-0 flex mx-8 lg:mr-0 lg:ml-8 xl:ml-12"
                     >
                       About
                     </a>
@@ -70,16 +47,7 @@ export default function Index() {
                   <li className="relative group">
                     <a
                       href="#services"
-                      className="
-                        menu-scroll
-                        text-base text-black
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:px-0
-                        flex mx-8
-                        lg:mr-0 lg:ml-8
-                        xl:ml-12
-                      "
+                      className=" menu-scroll text-base text-black group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:px-0 flex mx-8 lg:mr-0 lg:ml-8 xl:ml-12"
                     >
                       Services
                     </a>
@@ -87,16 +55,7 @@ export default function Index() {
                   <li className="relative group">
                     <a
                       href="#pricing"
-                      className="
-                        menu-scroll
-                        text-base text-black
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:px-0
-                        flex mx-8
-                        lg:mr-0 lg:ml-8
-                        xl:ml-12
-                      "
+                      className=" menu-scroll text-base text-black group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:px-0 flex mx-8 lg:mr-0 lg:ml-8 xl:ml-12"
                     >
                       Pricing
                     </a>
@@ -104,16 +63,7 @@ export default function Index() {
                   <li className="relative group">
                     <a
                       href="#contact"
-                      className="
-                        menu-scroll
-                        text-base text-black
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:px-0
-                        flex mx-8
-                        lg:mr-0 lg:ml-8
-                        xl:ml-12
-                      "
+                      className=" menu-scroll text-base text-black group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:px-0 flex mx-8 lg:mr-0 lg:ml-8 xl:ml-12"
                     >
                       Contact
                     </a>
@@ -121,47 +71,11 @@ export default function Index() {
                   <li className="relative group submenu-item">
                     <a
                       href="javascript:void(0)"
-                      className="
-                        text-base text-black
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:pl-0 lg:pr-4
-                        flex mx-8
-                        lg:mr-0 lg:ml-8
-                        xl:ml-12
-                        relative
-                        after:absolute
-                        after:w-2
-                        after:h-2
-                        after:border-b-2
-                        after:border-r-2
-                        after:border-current
-                        after:rotate-45
-                        lg:after:right-0
-                        after:right-1
-                        after:top-1/2
-                        after:translate-y-[-50%]
-                        after:mt-[-2px]
-                      "
+                      className=" text-base text-black group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:pl-0 lg:pr-4 flex mx-8 lg:mr-0 lg:ml-8 xl:ml-12 relative after:absolute after:w-2 after:h-2 after:border-b-2 after:border-r-2 after:border-current after:rotate-45 lg:after:right-0 after:right-1 after:top-1/2 after:translate-y-[-50%] after:mt-[-2px]"
                     >
                       Pages
                     </a>
-                    <div
-                      className="
-                        submenu
-                        hidden relative
-                        lg:absolute
-                        w-[250px] top-full
-                        lg:top-[110%]
-                        left-0 rounded-sm
-                        lg:shadow-lg
-                        p-4
-                        lg:block lg:opacity-0 lg:invisible
-                        group-hover:opacity-100
-                        lg:group-hover:visible lg:group-hover:top-full
-                        bg-white transition-[top] duration-300
-                      "
-                    >
+                    <div className=" submenu hidden relative lg:absolute w-[250px] top-full lg:top-[110%] left-0 rounded-sm lg:shadow-lg p-4 lg:block lg:opacity-0 lg:invisible group-hover:opacity-100 lg:group-hover:visible lg:group-hover:top-full bg-white transition-[top] duration-300">
                       <a
                         href="portfolio-details.html"
                         className="block text-sm text-black rounded hover:text-primary py-[10px] px-4
@@ -172,21 +86,13 @@ export default function Index() {
 
                       <a
                         href="blog-grids.html"
-                        className="
-                          block text-sm text-black rounded
-                          hover:text-primary
-                          py-[10px] px-4
-                        "
+                        className=" block text-sm text-black rounded hover:text-primary py-[10px] px-4"
                       >
                         Blog Grids Page
                       </a>
                       <a
                         href="blog-details.html"
-                        className="
-                          block text-sm text-black rounded
-                          hover:text-primary
-                          py-[10px] px-4
-                        "
+                        className=" block text-sm text-black rounded hover:text-primary py-[10px] px-4"
                       >
                         Blog Details Page
                       </a>
@@ -198,20 +104,7 @@ export default function Index() {
             <div className="sm:flex justify-end hidden pr-16 lg:pr-0">
               <a
                 href="#contact"
-                className="
-                  text-base
-                  font-bold
-                  text-white
-                  bg-primary
-                  rounded-full
-                  py-3
-                  px-8
-                  md:px-9
-                  lg:px-8
-                  xl:px-9
-                  hover:shadow-signUp hover:bg-opacity-90
-                  transition ease-in-out duration-300
-                "
+                className=" text-base font-bold text-white bg-primary rounded-full py-3 px-8 md:px-9 lg:px-8 xl:px-9 hover:shadow-signUp hover:bg-opacity-90 transition ease-in-out duration-300"
               >
                 Download CV
               </a>
