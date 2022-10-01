3429028504eb73cc235cca27d9a6975f242b011c
Luka Lazic <luka.web.php@gmail.com>
2022-10-01 14:32:05 +0200

    animate StyledTitle component

--- app/common/components/StyledTitle.tsx
+++ app/common/components/StyledTitle.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 interface IStyledTitle {
   title: string;
   preTitle?: string;
@@ -12,17 +14,33 @@ const StyledTitle: React.FC<IStyledTitle> = ({
   return (
     <div className="flex flex-wrap mx-[-16px]">
       <div className="w-full px-4">
-        <div className="max-w-[600px] mx-auto text-center mb-[70px]">
+        <motion.div
+          animate={{ y: 0 }}
+          initial={{ y: -50 }}
+          className="max-w-[600px] mx-auto text-center mb-[70px]"
+        >
           {preTitle && (
-            <span className="font-semibold text-lg text-primary block mb-2">
+            <motion.span
+              animate={{ left: 0 }}
+              initial={{ left: "100vw" }}
+              transition={{ delay: 0.3 }}
+              className="font-semibold text-lg text-primary block mb-2 relative"
+            >
               {preTitle}
-            </span>
+            </motion.span>
           )}
           <h2 className="font-bold text-black text-3xl sm:text-4xl md:text-[45px] mb-5">
             {title}
           </h2>
-          <p className="font-medium text-lg text-body-color">{description}</p>
-        </div>
+          <motion.p
+            animate={{ right: 0 }}
+            initial={{ right: "100vw" }}
+            transition={{ delay: 0.6 }}
+            className="font-medium text-lg text-body-color relative"
+          >
+            {description}
+          </motion.p>
+        </motion.div>
       </div>
     </div>
   );
