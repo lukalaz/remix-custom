e40ed756a56aa8968c60f9f11728c298d386a273
Luka Lazic <luka.web.php@gmail.com>
2022-10-09 02:17:33 +0200

    styling

--- app/features/homepage/components/Hero.tsx
+++ app/features/homepage/components/Hero.tsx
@@ -8,13 +8,13 @@ import {
 
 const Hero: React.FC = () => {
   return (
-    <div className="bg-gray-100">
+    <div className="bg-black">
       <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
         <motion.div
           {...snapFromTopAnimation}
           className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10"
         >
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-black leading-7 md:leading-10">
+          <h1 className="text-2xl text-white sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center font-black leading-7 md:leading-10">
             Luka Lazić
           </h1>
           <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">
