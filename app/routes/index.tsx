c182424bea6eefde2dbef6fb9f1dc81af1ffa69c
Luka Lazic <luka.web.php@gmail.com>
2022-10-09 02:13:29 +0200

    add hero section

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,11 @@
 import ContactMe from "~/features/homepage/components/ContactMe";
+import Hero from "~/features/homepage/components/Hero";
 import TechStack from "~/features/homepage/components/TechStack";
 
 const Index: React.FC = () => {
   return (
     <>
+      <Hero />
       <TechStack />
       <ContactMe />
     </>
