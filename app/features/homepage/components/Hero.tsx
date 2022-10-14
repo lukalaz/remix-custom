1f58ff21bb4c83263f254288b7f5bdb65960f066
Luka Lazic <luka.web.php@gmail.com>
2022-10-14 19:07:49 +0200

    add stars

--- app/features/homepage/components/Hero.tsx
+++ app/features/homepage/components/Hero.tsx
@@ -8,8 +8,12 @@ import {
 
 const Hero: React.FC = () => {
   return (
-    <div className="bg-black">
-      <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
+    <div className="bg-black overflow-hidden center-container items-center flex">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+
+      <div className="container mx-auto flex flex-col items-center py-12 sm:py-24 pb-24 sm:pb-48">
         <motion.div
           {...snapFromTopAnimation}
           className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10"
@@ -17,7 +21,7 @@ const Hero: React.FC = () => {
           <h1 className="text-2xl text-white sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center font-black leading-7 md:leading-10">
             Luka Lazić
           </h1>
-          <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">
+          <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center sm:text-lg">
             This is my personal website where I post my projects and write about
             web development and life. Check out some of my projects or my blog!
           </p>
