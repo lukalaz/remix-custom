5bf1c562fd21838152d6b1471202a7758383eae4
Luka Lazic <luka.web.php@gmail.com>
2022-11-12 17:52:51 +0100

    fix sendgrid email content and title

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -61,9 +61,9 @@ export const action: ActionFunction = async ({ request }) => {
   const msg = {
     to: "luka.web.php@gmail.com", // Change to your recipient
     from: "luka.web.php@gmail.com", // Change to your verified sender
-    subject: "Sending with SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+    subject: "Mail sa l-l.tech sajta",
+    text: message?.toString(),
+    html: message?.toString() || "",
   };
 
   const res = await Sendgrid.send(msg)
