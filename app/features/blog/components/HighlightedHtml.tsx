6709fc00a666778f4b7ab1b2865115219cfb1ec8
Luka Lazic <luka.web.php@gmail.com>
2023-03-12 20:10:47 +0100

    add code highlightning

---
+++ app/features/blog/components/HighlightedHtml.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from "react";
+import hljs from "highlight.js/lib/core";
+import typescript from "highlight.js/lib/languages/typescript";
+
+hljs.registerLanguage("typescript", typescript);
+
+interface IHighlightedHtmlContent {
+  htmlContent: string;
+}
+
+const HighlightedHtmlContent: React.FC<IHighlightedHtmlContent> = ({
+  htmlContent,
+}) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const codeBlocks = containerRef.current.querySelectorAll("code");
+
+      codeBlocks.forEach((block) => {
+        const languageMatch = block.className.match(/language-(\w+)/);
+        const language = languageMatch ? languageMatch[1] : undefined;
+
+        if (language) {
+          hljs.highlightBlock(block);
+        }
+      });
+    }
+  }, [htmlContent]);
+
+  return (
+    <div
+      ref={containerRef}
+      dangerouslySetInnerHTML={{
+        __html: htmlContent.replace(
+          /href/g,
+          "target='_blank' class='hover-underline-animation hover:text-white transition-all' href"
+        ),
+      }}
+    />
+  );
+};
+
+export { HighlightedHtmlContent };
