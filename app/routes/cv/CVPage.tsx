ab797d04498119dabe301dee70c3e511bead9694
Luka Lazic <luka.web.php@gmail.com>
2023-10-23 21:38:26 +0200

    update CV page text

--- app/routes/cv/CVPage.tsx
+++ app/routes/cv/CVPage.tsx
@@ -54,12 +54,14 @@ const CVPage: React.FC = () => {
               <div className="flex justify-between">
                 <div className="text-gray-400">Experience</div>
                 <div className="font-medium text-right text-white">
-                  3+ years
+                  9+ years
                 </div>
               </div>
               <div className="flex justify-between">
                 <div className="text-gray-400">Availability</div>
-                <div className="font-medium text-right text-white">1 week</div>
+                <div className="font-medium text-right text-white">
+                  10h per week
+                </div>
               </div>
               <div className="flex justify-between">
                 <div className="text-gray-400">Relocation</div>
@@ -133,7 +135,7 @@ const CVPage: React.FC = () => {
                 <div className="item-header">
                   <div className="space-y-1.5">
                     <div className="font-medium text-white text-md">
-                      Senior Developer
+                      Frontend Developer
                     </div>
                     <div className="flex space-x-5">
                       <div className="item-header-info">
@@ -151,7 +153,7 @@ const CVPage: React.FC = () => {
                             d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                           ></path>
                         </svg>
-                        <span>Twitter</span>
+                        <span>Nul Tien</span>
                       </div>
                       <div className="item-header-info">
                         <svg
@@ -174,7 +176,7 @@ const CVPage: React.FC = () => {
                             d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                           ></path>
                         </svg>
-                        <span>London</span>
+                        <span>Belgrade</span>
                       </div>
                     </div>
                   </div>
@@ -195,16 +197,40 @@ const CVPage: React.FC = () => {
                           d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                         ></path>
                       </svg>
-                      <span>June 2019 – Present</span>
+                      <span>Mar 2021 - Present</span>
                     </div>
                   </div>
                 </div>
                 <p className="text-white">
-                  Aut omnis voluptatem sit sequi. Voluptatem temporibus repellat
-                  voluptatem voluptatibus enim voluptas necessitatibus. Aut
-                  quasi sunt dolor. Commodi dolores saepe asperiores beatae
-                  voluptate corporis est ea voluptatem. Enim quo sed et sint
-                  aspernatur distinctio qui quam.
+                  <span className="mb-2 block">
+                    Within my role at Nul Tien, I am actively engaged with the
+                    BOSS portal—a versatile front-end application designed to
+                    empower employees in managing their work effectively. This
+                    dynamic platform enables us to track project-specific time
+                    allocation, create schedules, execute contracts, and
+                    meticulously oversee our interactions with various client
+                    organizations. It also facilitates the management of vital
+                    details, including contact persons and important documents.
+                  </span>
+                  <span className="mb-2 block">
+                    In tandem with the desktop application, I am involved in the
+                    development of the ERA app, a mobile counterpart to the
+                    portal. Built using React Native and interfacing seamlessly
+                    with the same APIs, the ERA app ensures that our
+                    functionality remains accessible and efficient on mobile
+                    devices.
+                  </span>
+                  <span className="mb-2 block">
+                    In house we have a custom written React components library
+                    and a React table library that I am contributing to when
+                    there is time. We use those in both our internal and
+                    commercial projects.
+                  </span>
+                  <span className="mb-2 block">
+                    Furthermore, I've been exploring the world of C# and the
+                    .NET framework, broadening my skill set to complement our
+                    tech stack.
+                  </span>
                 </p>
                 <div className="border-b border-gray-200"></div>
               </div>
@@ -219,7 +245,7 @@ const CVPage: React.FC = () => {
                 <div className="item-header">
                   <div className="space-y-1.5">
                     <div className="font-medium text-white text-md">
-                      Middle Developer
+                      Web and Front-end Developer
                     </div>
                     <div className="flex space-x-5">
                       <div className="item-header-info">
@@ -237,7 +263,7 @@ const CVPage: React.FC = () => {
                             d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                           ></path>
                         </svg>
-                        <span>Dropbox</span>
+                        <span>BlueGrid</span>
                       </div>
                       <div className="item-header-info">
                         <svg
@@ -260,7 +286,7 @@ const CVPage: React.FC = () => {
                             d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                           ></path>
                         </svg>
-                        <span>London</span>
+                        <span>Belgrade</span>
                       </div>
                     </div>
                   </div>
@@ -281,22 +307,44 @@ const CVPage: React.FC = () => {
                           d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                         ></path>
                       </svg>
-                      <span>April 2018 – June 2019</span>
+                      <span>March 2019 - February 2021</span>
                     </div>
                   </div>
                 </div>
                 <p className="text-white">
-                  Aut omnis voluptatem sit sequi. Voluptatem temporibus repellat
-                  voluptatem voluptatibus enim voluptas necessitatibus. Aut
-                  quasi sunt dolor. Commodi dolores saepe asperiores beatae
-                  voluptate corporis est ea voluptatem. Enim quo sed et sint
-                  aspernatur distinctio qui quam.
+                  <span className="mb-2 block">
+                    During my time at BlueGrid, I contributed to the MyAccident
+                    platform, a dynamic enterprise focused on assisting
+                    individuals across the United States in obtaining crucial
+                    information about their car accidents and securing legal
+                    support. The platform's initial foundation was established
+                    using WordPress, a foundation that we later adeptly
+                    transitioned to the Express framework. This allowed for
+                    improved performance and scalability.
+                  </span>
+
+                  <span className="mb-2 block">
+                    One of the defining aspects of my role involved managing a
+                    robust MongoDB database housing extensive accident-related
+                    data. This repository was regularly updated by various
+                    agencies, each using distinct data formats, making the
+                    database integration an intricate and essential part of our
+                    operations.
+                  </span>
+
+                  <span className="mb-2 block">
+                    Additionally, I ventured into the world of React
+                    development, where I played an integral role in a discrete,
+                    internal project. This project now resides on the AWS
+                    platform, demonstrating my adaptability and versatility in
+                    tackling diverse technologies and platforms.
+                  </span>
                 </p>
                 <div className="border-b border-gray-200"></div>
               </div>
             </div>
 
-            <div className="item-section">
+            <div className="mb-5 item-section">
               <div className="company-logo">
                 <i className="bx bxl-blogger text-3xl"></i>
               </div>
@@ -305,7 +353,7 @@ const CVPage: React.FC = () => {
                 <div className="item-header">
                   <div className="space-y-1.5">
                     <div className="font-medium text-white text-md">
-                      Junior Developer
+                      Web Developer
                     </div>
                     <div className="flex space-x-5">
                       <div className="item-header-info">
@@ -323,7 +371,7 @@ const CVPage: React.FC = () => {
                             d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                           ></path>
                         </svg>
-                        <span>Blogger</span>
+                        <span>GuitarZoom</span>
                       </div>
                       <div className="item-header-info">
                         <svg
@@ -346,7 +394,7 @@ const CVPage: React.FC = () => {
                             d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                           ></path>
                         </svg>
-                        <span>London</span>
+                        <span>Remote</span>
                       </div>
                     </div>
                   </div>
@@ -367,18 +415,123 @@ const CVPage: React.FC = () => {
                           d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                         ></path>
                       </svg>
-                      <span>December 2017 – March 2018</span>
+                      <span>September 2016 - March 2018</span>
                     </div>
                   </div>
                 </div>
                 <p className="text-white">
-                  Aut omnis voluptatem sit sequi. Voluptatem temporibus repellat
-                  voluptatem voluptatibus enim voluptas necessitatibus. Aut
-                  quasi sunt dolor. Commodi dolores saepe asperiores beatae
-                  voluptate corporis est ea voluptatem. Enim quo sed et sint
-                  aspernatur distinctio qui quam.
+                  <span className="mb-2 block">
+                    Guitarzoom is an online learning platform for guitar
+                    players. While working there, I have worked on our main
+                    website and members area website (used by 10k+ users) which
+                    both were running on WordPress (back then at least) as well
+                    as on many stand-alone PHP/JS pages used for marketing and
+                    sales purposes.
+                  </span>
+                  <span className="mb-2 block">
+                    Since the company was mostly marketing-driven, the
+                    environment was dynamic and we experimented with many
+                    different third-party marketing, tracking and sales
+                    platforms. My main job was building and maintaining the
+                    WordPress website as well as the marketing pages and
+                    integrating everything with our various marketing tools and
+                    platforms. We made lots of last minute changes before
+                    promotions go live so I have learned to be flexible and
+                    quick with my work.
+                  </span>
                 </p>
+                <div className="border-b border-gray-200"></div>
               </div>
+            </div>
+
+            <div className="w-full space-y-5 ml-0">
+              <div className="item-header">
+                <div className="space-y-1.5">
+                  <div className="font-medium text-white text-md">
+                    Web Developer
+                  </div>
+                  <div className="flex space-x-5">
+                    <div className="item-header-info">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                        ></path>
+                      </svg>
+                      <span>GuitarZoom</span>
+                    </div>
+                    <div className="item-header-info">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                        ></path>
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                        ></path>
+                      </svg>
+                      <span>Remote</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="space-y-2 sm:text-right">
+                  <div className="text-white">Full time</div>
+                  <div className="item-header-info">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                      ></path>
+                    </svg>
+                    <span>September 2014 - August 2016</span>
+                  </div>
+                </div>
+              </div>
+              <p className="text-white">
+                <span className="mb-2 block">
+                  I was a part of Legally Concealed, a digital marketing firm
+                  specializing in the distribution of on-demand self-defense and
+                  home-defense videos for the U.S. audience. My position
+                  primarily revolved around being a WordPress and PHP developer,
+                  with close collaboration with the marketing team, which
+                  constituted the majority of the company's workforce.
+                </span>
+                <span className="mb-2 block">
+                  Within the framework of a small and agile remote team, we
+                  thrived in a high-speed work environment. My central
+                  responsibilities encompassed the development and upkeep of the
+                  WordPress website, crafting standalone PHP sales pages, and
+                  creating seamless integrations with third-party software for
+                  customer behavior tracking and sales analytics.
+                </span>
+              </p>
             </div>
           </div>
 
@@ -390,6 +543,9 @@ const CVPage: React.FC = () => {
                   <div className="space-y-1.5">
                     <div className="font-medium text-white text-md">
                       Bachelor of Computer Science
+                    </div>
+                    <div className="font-medium text-white text-xs">
+                      No Degree
                     </div>
                     <div className="flex space-x-5">
                       <div className="item-header-info">
@@ -409,7 +565,7 @@ const CVPage: React.FC = () => {
                             d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
                           ></path>
                         </svg>
-                        <span>Brunel University</span>
+                        <span>Faculty of Mathematics</span>
                       </div>
                       <div className="item-header-info">
                         <svg
@@ -432,7 +588,7 @@ const CVPage: React.FC = () => {
                             d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                           ></path>
                         </svg>
-                        <span>London</span>
+                        <span>Belgrade</span>
                       </div>
                     </div>
                   </div>
@@ -452,11 +608,10 @@ const CVPage: React.FC = () => {
                           d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                         ></path>
                       </svg>
-                      <span>September 2017 – May 2019</span>
+                      <span>2009 - 2014</span>
                     </div>
                   </div>
                 </div>
-                <div className="border-b border-gray-200"></div>
               </div>
             </div>
           </div>
