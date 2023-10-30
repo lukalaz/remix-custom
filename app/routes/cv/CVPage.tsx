b560679abf46fe4d9f174fcbb32360bdf29abb86
Luka Lazic <luka.web.php@gmail.com>
2023-10-30 19:57:59 +0100

    allow download on button click

--- app/routes/cv/CVPage.tsx
+++ app/routes/cv/CVPage.tsx
@@ -37,7 +37,11 @@ const CVPage: React.FC = () => {
                 >
                   <div>Download CV</div>
                 </a>
-                <button className="download-btn-icon bg-primary">
+                <a
+                  href="assets/cv/CV_Luka-Lazic.pdf"
+                  target="_blank"
+                  className="download-btn-icon bg-primary"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
@@ -52,7 +56,7 @@ const CVPage: React.FC = () => {
                       d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
                     ></path>
                   </svg>
-                </button>
+                </a>
               </div>
             </div>
           </motion.div>
